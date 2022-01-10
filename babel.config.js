8309c94b57137139ef771d4db8dd025bed5c1b58
virgoone <me@virgo.one>
2022-01-10 15:49:38 +0800

    fix: 🐛 demo 访问出错问题

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,15 @@
 module.exports = () => ({
+  overrides: true,
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        loose: true,
+      },
+    ],
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+  ],
   env: {
     development: {
       plugins: [require.resolve('@emotion/babel-plugin')],
